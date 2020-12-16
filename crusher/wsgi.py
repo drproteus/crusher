9eb73f96934fe6678da13187430f1618e3d6611f
Jake Goritski <jgoritski@gmail.com>
2020-12-16 13:13:50 -0500

    formatting

--- crusher/wsgi.py
+++ crusher/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crusher.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crusher.settings")
 
 application = get_wsgi_application()
