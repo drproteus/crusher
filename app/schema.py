974f547867317e9378fade552018375fa95fd642
Jake Goritski <jgoritski@gmail.com>
2020-12-17 10:46:04 -0500

    updating graphql schema

--- app/schema.py
+++ app/schema.py
@@ -7,8 +7,9 @@ from app.models import (
     Credit as CreditModel,
     SKU as SKUModel,
     Client as ClientModel,
-    Staff as StaffModel,
-    Item as ItemModel,
+    Vessel as VesselModel,
+    Request as RequestModel,
+    Job as JobModel,
 )
 
 
@@ -17,19 +18,32 @@ class Client(DjangoObjectType):
         model = ClientModel
 
 
-class Staff(DjangoObjectType):
+class Vessel(DjangoObjectType):
     class Meta:
-        model = StaffModel
+        model = VesselModel
 
-    name = graphene.String()
+    client = graphene.Field(Client)
 
-    def resolve_name(self, info):
-        return self.name
+    def resolve_client(self, info):
+        return self.client
 
 
-class Item(DjangoObjectType):
+class Request(DjangoObjectType):
     class Meta:
-        model = ItemModel
+        model = RequestModel
+
+    client = graphene.Field(Client)
+
+    def resolve_client(self, info):
+        return self.client
+
+
+class Job(DjangoObjectType):
+    class Meta:
+        model = JobModel
+
+    vessel = graphene.Field(Vessel)
+    origin_request = graphene.Field(Request)
 
 
 class SKU(DjangoObjectType):
@@ -60,8 +74,17 @@ class Invoice(DjangoObjectType):
     credits = graphene.List(Credit)
     client = graphene.Field(Client)
 
+    job = graphene.Field(Job)
+
+    transportation = graphene.List(LineItem)
+    staff = graphene.List(LineItem)
+    items = graphene.List(LineItem)
+
     def resolve_client(self, info):
         return self.client
+
+    def resolve_job(self, info):
+        return self.job
 
     def resolve_line_items(self, info):
         return self.line_items.all()
@@ -69,12 +92,38 @@ class Invoice(DjangoObjectType):
     def resolve_credits(self, info):
         return self.credits.all()
 
+    def resolve_transportation(self, info):
+        return self.line_items.filter(sku__metadata__type="transport")
+
+    def resolve_staff(self, info):
+        return self.line_items.filter(sku__metadata__type="staff")
+
+    def resolve_item(self, info):
+        return self.line_items.filter(sku__metadata__type="item")
+
 
 class Query(graphene.ObjectType):
     invoices = graphene.List(Invoice)
+    skus = graphene.List(SKU)
+    transport_skus = graphene.List(SKU)
+    staff_skus = graphene.List(SKU)
+    item_skus = graphene.List(SKU)
+
 
     def resolve_invoices(self, info):
         return InvoiceModel.objects.all()
+
+    def resolve_skus(self, info):
+        return SKUModel.objects.all()
+
+    def resolve_transport_skus(self, info):
+        return SKUModel.transportation.all()
+
+    def resolve_staff_skus(self, info):
+        return SKUModel.staff.all()
+
+    def resolve_item_skus(self, info):
+        return SKUModel.items.all()
 
 
 schema = graphene.Schema(query=Query)
