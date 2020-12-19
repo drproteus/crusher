b5d154cde8e956f58c95f789c262927d1acc2e55
Jake Goritski <jgoritski@gmail.com>
2020-12-18 23:48:14 -0500

    add bootstrap

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
 
+import Container from 'react-bootstrap/Container';
+
 import { ApolloProvider } from '@apollo/react-hooks';
 import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
-import { SKUs, Clients, Contacts, Home } from "./components.jsx";
+import { SKUs, Clients, Contacts, Home, MainNav } from "./components.jsx";
 
 import {
   BrowserRouter as Router,
@@ -30,58 +32,45 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <div>
-      <h2>CRUSHER 🚀</h2>
-    </div>
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/skus">SKUs</Link>
-            </li>
-            <li>
-              <Link to="/contacts">Contacts</Link>
-            </li>
-            <li>
-              <Link to="/clients">Clients</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route path="/skus/by-type/:skuType">
-            <SKUs />
-          </Route>
-          <Route path="/skus/by-tag/:tag">
-            <SKUs />
-          </Route>
-          <Route path="/skus/:id">
-            <SKUs />
-          </Route>
-          <Route path="/skus">
-            <SKUs />
-          </Route>
-          <Route path="/contacts/:id">
-            <Contacts />
-          </Route>
-          <Route path="/contacts">
-            <Contacts />
-          </Route>
-          <Route path="/clients/:id">
-            <Clients />
-          </Route>
-          <Route path="/clients">
-            <Clients />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
+      <Container fluid className="p-3">
+        <h2>CRUSHER 🚀</h2>
+        <MainNav></MainNav>
+        <Container fluid className="p-3">
+          <Switch>
+            <Route path="/skus/by-type/:skuType">
+              <h3>SKUs by Type</h3>
+              <SKUs />
+            </Route>
+            <Route path="/skus/by-tag/:tag">
+              <h3>SKUs by Tag</h3>
+              <SKUs />
+            </Route>
+            <Route path="/skus/:id">
+              <SKUs />
+            </Route>
+            <Route path="/skus">
+              <h3>All SKUs</h3>
+              <SKUs />
+            </Route>
+            <Route path="/contacts/:id">
+              <Contacts />
+            </Route>
+            <Route path="/contacts">
+              <Contacts />
+            </Route>
+            <Route path="/clients/:id">
+              <Clients />
+            </Route>
+            <Route path="/clients">
+              <Clients />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </Container>
     </Router>
   </ApolloProvider>
 );
--- frontend/src/components.jsx
+++ frontend/src/components.jsx
@@ -1,64 +1,122 @@
 import React from 'react';
 import { useParams, Link } from "react-router-dom";
 import { useQuery } from '@apollo/react-hooks';
+import Nav from 'react-bootstrap/Nav';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 import { SKUS, CONTACTS, CLIENTS } from "./queries.jsx";
 
 
 function Clients() {
-  const { loading, error, data } = useQuery(CLIENTS, { variables: useParams() });
+    const { loading, error, data } = useQuery(CLIENTS, { variables: useParams() });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  return data.clients.edges.map(({ node }) => (
-    <div key={node.clientId}>
-      <p>
-        {node.company}: {node.clientId}
-      </p>
-    </div>
-  ));
+    return data.clients.edges.map(({ node }) => (
+        <div key={node.clientId}>
+            <p>
+                {node.company}: {node.clientId}
+            </p>
+        </div>
+    ));
 }
 
 
 function Contacts() {
-  const { loading, error, data } = useQuery(CONTACTS, { variables: useParams() });
+    const { loading, error, data } = useQuery(CONTACTS, { variables: useParams() });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  return data.contacts.edges.map(({ node }) => (
-    <div key={node.contactId}>
-      <p>
-        {node.fullname}: <Link to={`/contacts/${node.contactId}`}>{node.contactId}</Link>
-      </p>
-      <Metadata inner={node.metadata}></Metadata>
-    </div>
-  ));
+    return data.contacts.edges.map(({ node }) => (
+        <div key={node.contactId}>
+            <p>
+                {node.fullname}: <Link to={`/contacts/${node.contactId}`}>{node.contactId}</Link>
+            </p>
+            <Metadata inner={node.metadata}></Metadata>
+        </div>
+    ));
 }
 
-function Metadata(data) {
-  return <div><pre>{JSON.stringify(data.inner, null, 2)}</pre></div>
+function Metadata({ inner }) {
+    return <div><pre>{JSON.stringify(inner, null, 2)}</pre></div>
 }
 
 
-function SKUs() {
-  const { loading, error, data } = useQuery(SKUS, { variables: useParams() });
+function SKURows() {
+    const { loading, error, data } = useQuery(SKUS, { variables: useParams() });
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
 
-  return data.skus.edges.map(({ node }) => (
-    <div key={node.skuId}>
-      <p><b>${node.defaultPrice}</b> -- {node.name || "unknown"} -- <em>{node.skuId}</em></p>
-      <Metadata inner={node.metadata}></Metadata>
-    </div>
-  ));
+    return data.skus.edges.map(({ node }) => (
+        <SKURow node={node}></SKURow>
+    ));
 }
 
 
 function Home() {
-  return <div class="jumbotron">crusher.beta</div>
+    return <div class="jumbotron">crusher.beta</div>
 }
 
-export {Clients, Contacts, Metadata, SKUs, Home};
+export { Clients, Contacts, Metadata, SKUs, Home, MainNav };
+
+
+function MainNav() {
+    return <Nav fill variant="pills" activeKey={"/" + window.location.pathname.split("/")[1]}>
+        <Nav.Item>
+            <Nav.Link href="/">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link href="/skus">SKUs</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link href="/contacts">Contacts</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+            <Nav.Link href="/clients">Clients</Nav.Link>
+        </Nav.Item>
+    </Nav>;
+}
+
+function SKUs() {
+    return <Table striped bordered hover>
+        <thead>
+            <tr>
+                <th>SKU ID</th>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Units</th>
+                <th>Default Price</th>
+                <th>Default Quantity</th>
+                <th>Minimum Quantity</th>
+                <th>Maximum Quantity</th>
+                <th>Minimum Price</th>
+                <th>Maximum Price</th>
+                <th>Metadata</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <SKURows></SKURows>
+    </Table>
+}
+
+function SKURow({ node }) {
+    return <tr>
+        <td>{node.skuId}</td>
+        <td>{node.name || node.metadata.name || "???"}</td>
+        <td>{node.type}</td>
+        <td>{node.units}</td>
+        <td>{node.defaultPrice}</td>
+        <td>{node.defaultQuantity}</td>
+        <td>{node.minimumQuantity}</td>
+        <td>{node.maximumQuantity}</td>
+        <td>{node.minimumPrice}</td>
+        <td>{node.maximumPrice}</td>
+        <td><Metadata inner={node.metadata}></Metadata></td>
+        <td><ButtonGroup><Button variant="danger">Delete</Button><Button varian="info">Edit</Button></ButtonGroup></td>
+    </tr>
+}
